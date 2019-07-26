bda79d1d92dc8bc0980cbbeae411817923f33240
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-07-26 17:57:30 +0530

    Step 1 : Gatsby building blocks

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from '../components/header';
+
+const About = () => {
+    
+    return(
+        <div style={{color:'teal'}}>             
+            <Header headerText = 'About Gatsby' />
+            <p>Such wow. Very React.</p>
+        </div>
+    )
+}
+
+export default About;
